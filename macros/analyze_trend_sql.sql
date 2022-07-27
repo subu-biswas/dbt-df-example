5b50af54c20252652cec913be5ba9bf2554439c5
Darshan Rathi <darshan.rathi@datafacade.onmicrosoft.com>
2022-07-27 11:38:56 +0530

    changs

--- macros/analyze_trend_sql.sql
+++ macros/analyze_trend_sql.sql
@@ -1,5 +1,5 @@
 {% macro analyze_trends(input_table, dim_column, target_column, agg_function)}
 
-select *, {{ agg_function }}({{ target_column }}) from {{ input_table }} group by {{ dim_column }}
+select {{ agg_function }}({{ target_column }}) AS "result", {{ dim_column }} from {{ input_table }} group by {{ dim_column }}
 
 {%endmacro}
