40cdd72f6724dbe7f65bfe094d79144fe2b946e9
Subu <subrata.iitr@gmail.com>
2022-07-27 17:26:04 -0700

    Initial changes

---
+++ macros/investigate_sales_drop.sql
@@ -0,0 +1,58 @@
+{% macro investigate_sales_drop(region, product) %}
+
+WITH Sales_Agg AS {
+    SELECT
+        SUM(m_amount) AS sum_sales_amount,
+        dim_region
+    FROM {{ ref("fct_sales") }}
+    WHERE dim_region = region
+    GROUP BY dim_region, ds
+},
+Agg_Customer_Support_Case AS {
+    SELECT
+        COUNT(1) AS count_case,
+        dim_region
+    FROM {{ ref("fct_customer_support_cases") }}
+    WHERE dim_region = region
+    GROUP BY dim_region, ds
+},
+Agg_Campaign AS {
+    SELECT
+        COUNT(1) as count_campaign,
+        dim_region
+    FROM {{ ref("fct_campaign") }}
+    WHERE dim_region = region
+    GROUP BY dim_region, ds
+},
+Agg_Diff_With_Competitor_Price AS {
+    SELECT
+        SUM(m_competing_price - m_price) as diff_price,
+        dim_region
+    FROM {{ ref("fct_competing_product") }}
+    WHERE
+        id_product = product AND
+        dim_region = region
+    GROUP BY dim_region, ds
+}
+SELECT
+    sum_sales_amount,
+    count_campaign,
+    diff_price,
+    count_case,
+    dim_region,
+    ds,
+    *
+FROM
+    Sales_Agg A
+LEFT JOIN
+    Agg_Customer_Support_Case B
+    ON A.dim_region = B.dim_region AND A.ds = B.ds
+LEFT JOIN
+    Agg_Campaign C
+    ON A.dim_region = C.dim_region AND A.ds = B.ds
+LEFT JOIN
+    Agg_Diff_With_Competitor_Price D
+    ON A.dim_region = D.dim_region AND A.ds = D.ds
+
+
+{% endmacro %}
