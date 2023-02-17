122f6da79090656472ad3fbeefb566847eca7070
root <root@df-dbt-service-5479cd5ff4-7h487>
2023-02-17 19:37:42 +0000

    vs2.0

--- df/AutoFlows/python/timeseries_forecast.py
+++ df/AutoFlows/python/timeseries_forecast.py
@@ -6,9 +6,13 @@ from dft import df_plot
 df = df_helper.get_table(parameter_name="input_table", parameter_display_name="Select The Table", parameter_description="Input table.")
 target_column = df_helper.get_column(parameter_name="target_column", parameter_display_name="Select The Time Series Column", parameter_description="Please select the time series column on which forecast Will be generated.")
 dimension_col = df_helper.get_column("dimension_col","Groupby","Plese specify it if want forecast based on a specific category else not required.")
-forecast_period = df_helper.get_string("forcast_period", "Forecast Period", "Please select the forecast period")
-frequency = df_helper.get_string("fequency", "Forequency Of The Forecast Period", "Please select frrquecy of the forecast period")
+forecast_period = df_helper.get_integer("forcast_period", "Forecast Period", "Please select the forecast period")
+frequency = df_helper.get_string("fequency", "Forequency Of The Forecast Period", "Please select frquecy of the forecast period")
 filter_query = df_helper.get_string("filter_query", "Specify Any SQL Query For Filtering", "Please specify any sql query for filtering.")
+
+# CONFIGS
+num_row_threshold = 100
+num_cat_threshold = 5
 
 metadata = df_helper.get_table_metadata("input_table")
 
@@ -16,10 +20,10 @@ table_tags = metadata.Tags
 def data_filter(df: pd.DataFrame, sql):
     try:
         df = df.query(f'{sql}')
-        except Exception as e:
-            print("Malformed SQL. Filtering aborted")
-            print(e)
-        return df
+    except Exception as e:
+        print("Malformed SQL. Filtering aborted")
+        print(e)
+    return df
 frequency_option_to_string = {
             'Months': 'MS',
             'Calendar Days': 'D',
@@ -31,7 +35,15 @@ frequency_option_to_string = {
             'Years': 'Y'
         }
 frequency = frequency_option_to_string.get(frequency)
+print(frequency)
 df = data_filter(df, filter_query)
+if dimension_col == None:
+    pass
+else:
+    if df[dimension_col].nunique() <= 5:
+        pass
+    else:
+        raise "Number of categories should not be greater than 5 , please use SQL query for filterin the data"
 if "TimeSeriesTable" in table_tags:
     for c in metadata.Columns:
         c_tag = c.Tags
@@ -65,30 +77,34 @@ elif "TimeAndValueTable" in table_tags:
     ts_df['variable'] = pd.to_datetime(ts_df['variable'])
     ts_df = ts_df.rename({'variable': timestamp_column}, axis=1)
     ts_df.set_index('variable').resample(frequency).median()
-elif "TimeSeriesWithDimensionTable":
+elif "TimeSeriesTableWithDimensions" in table_tags:
     for c in metadata.Columns:
         c_tag = c.Tags
-        if 'TimestampColumn' in c_tag:
+        if "TimestampColumn" in c_tag:
             timestamp_column = c.Name
             break
     df[timestamp_column] = pd.to_datetime(df[timestamp_column])
     ts_df = df.pivot(columns=dimension_col, values=target_column)
     ts_df[timestamp_column] = df[timestamp_column]
     ts_df = ts_df.groupby([timestamp_column]).median().reset_index()
-    
     ts_df = ts_df.set_index(timestamp_column).resample(frequency).median()
+    
 else:
     raise "Forecast not possible for this table"
 
 #Forecast Generation
-trimmed_df = ts_df.tail(min(3000, len(df)))
+if len(df[target_column]) < num_row_threshold:
+    raise "Number of datapoint is very low for generating forecast"
+trimmed_df = ts_df.tail(min(3000, len(df[target_column])))
 primary_dimension_values = ts_df.columns.tolist()
 forecast_df1 = pd.DataFrame()
 for primary_dimension_value in primary_dimension_values:
      # ----fitting prophet model
-     print('Please Wait. Fitting Timeseries Model For'+primary_dimension_value)
+     print('Please Wait. Fitting Timeseries Model For '+primary_dimension_value)
      time_series = trimmed_df[primary_dimension_value].copy()
      time_series = pd.DataFrame(time_series.reset_index())
+     timestamp_column = time_series.columns[0]
+     target_column =  time_series.columns[1]
      time_series.rename(columns={timestamp_column: 'ds', target_column: 'y'}, inplace=True)
      model = Prophet(interval_width=0.95)
 
@@ -99,13 +115,12 @@ for primary_dimension_value in primary_dimension_values:
      future = model.make_future_dataframe(periods=forecast_period, freq=frequency)
      forecast = model.predict(future)[['ds', 'yhat']][-forecast_period:]
      forecast.rename(columns={'yhat': 'y'}, inplace=True)
-
-     forecast_df = trimmed_df.append(forecast)
+     forecast_df = time_series.append(forecast)
      forecast_df.rename(columns={'ds': timestamp_column, 'y': target_column}, inplace=True)
      forecast_df = forecast_df.set_index(timestamp_column)
 
-     forecast_df1 = df.append(forecast_df, axis = 1)
+     forecast_df1 = forecast_df1.append(forecast_df, ignore_index=True)
 
-df_plot.time_series_forecast("Time Series Forecast", forecast_period, forecast_df1)
+df_plot.time_series_forecast("Time Series Forecast", forecast_period, forecast_df1[-30+forecast_period:])
 
-df_helper.publish(forecast_df1.head())
+df_helper.publish(forecast_df1)
